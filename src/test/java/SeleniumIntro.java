87b1438ff89960d6aa1c5ecff2e6410a90127cc2
rfnshare <rfnshare@gmail.com>
2021-12-11 13:59:49 +0600

    Basic Selenium Commands

--- src/test/java/SeleniumIntro.java
+++ src/test/java/SeleniumIntro.java
@@ -9,11 +9,13 @@ import java.util.concurrent.TimeUnit;
 
 public class SeleniumIntro {
     public static void main(String[] args) {
-//        ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\IdeaProjects\\JavaSeleniumBasic\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\IdeaProjects\\JavaSeleniumBasic\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://phptravels.com/demo/");
+        driver.get("https://rahulshettyacademy.com/");
         driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
@@ -21,6 +23,6 @@ public class SeleniumIntro {
             // handle the exception...
             // For example consider calling Thread.currentThread().interrupt(); here.
         }
-        driver.close();
+        driver.quit();
     }
 }
