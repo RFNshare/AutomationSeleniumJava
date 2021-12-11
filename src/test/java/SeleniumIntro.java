d21a6e137db17c0a36cced27274e9dccd8598cf2
rfnshare <rfnshare@gmail.com>
2021-12-11 14:07:31 +0600

    Basic Selenium Commands Firefox & Edge

--- src/test/java/SeleniumIntro.java
+++ src/test/java/SeleniumIntro.java
@@ -1,17 +1,22 @@
 import io.github.bonigarcia.wdm.config.DriverManagerType;
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import io.github.bonigarcia.wdm.managers.EdgeDriverManager;
 import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class SeleniumIntro {
     public static void main(String[] args) {
-        ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
+//        ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
+//        FirefoxDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+        EdgeDriverManager.getInstance(DriverManagerType.EDGE).setup();
 //        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\IdeaProjects\\JavaSeleniumBasic\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new EdgeDriver();
         driver.get("https://rahulshettyacademy.com/");
         driver.manage().window().maximize();
         System.out.println(driver.getTitle());
